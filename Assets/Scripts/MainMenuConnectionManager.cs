61343393e5a44aacc2c90c60289412e2abeb26f0
Lior Hadashian <lior@gavra-games.com>
2024-05-21 21:16:38 +0300

    Added initial connection to Photon

--- Assets/Scripts/MainMenuConnectionManager.cs
+++ Assets/Scripts/MainMenuConnectionManager.cs
@@ -1,7 +1,25 @@
+using System;
 using Photon.Pun;
+using TMPro;
 using UnityEngine;
 
 public class MainMenuConnectionManager : MonoBehaviourPunCallbacks
 {
+    [SerializeField] private TextMeshProUGUI debugPhotonText;
     
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("We successfuly connected to Photon");
+        base.OnConnectedToMaster();
+    }
+
+    private void Update()
+    {
+        debugPhotonText.text = PhotonNetwork.NetworkClientState.ToString();
+    }
 }
