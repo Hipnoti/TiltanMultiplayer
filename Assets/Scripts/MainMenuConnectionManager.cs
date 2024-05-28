8638599a73868dde09ef046d3b3223dcfba4ce98
Lior Hadashian <lior@gavra-games.com>
2024-05-28 03:52:54 +0300

    Added room creation and lobby functionality

--- Assets/Scripts/MainMenuConnectionManager.cs
+++ Assets/Scripts/MainMenuConnectionManager.cs
@@ -1,13 +1,16 @@
 using System;
+using System.Collections.Generic;
 using Photon.Pun;
+using Photon.Realtime;
 using TMPro;
 using UnityEngine;
 
 public class MainMenuConnectionManager : MonoBehaviourPunCallbacks
 {
+    private const string LOBBY_DEFAULT_NAME = "OurCoolLobby";
     [SerializeField] private TextMeshProUGUI debugPhotonText;
-    
-    private void Start()
+
+    public void Connect()
     {
         PhotonNetwork.ConnectUsingSettings();
     }
@@ -16,6 +19,39 @@ public class MainMenuConnectionManager : MonoBehaviourPunCallbacks
     {
         Debug.Log("We successfuly connected to Photon");
         base.OnConnectedToMaster();
+    }
+
+
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom("MyRoom");
+    }
+
+    public override void OnCreatedRoom()
+    {
+        base.OnCreatedRoom();
+        Debug.Log("Room created successfully!");
+    }
+
+    public void JoinLobby()
+    {
+        PhotonNetwork.JoinLobby(new TypedLobby(LOBBY_DEFAULT_NAME, LobbyType.Default));
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        Debug.Log($"We successfully joined the lobby {PhotonNetwork.CurrentLobby}!");
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        base.OnRoomListUpdate(roomList);
+        foreach (RoomInfo roomInfo in roomList)
+        {
+            Debug.Log(roomInfo.Name);
+        }
     }
 
     private void Update()
