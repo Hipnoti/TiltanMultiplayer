a73cc02abb8054d93434f5faed87ccf5d806c58f
Lior Hadashian <lior@gavra-games.com>
2024-06-18 16:05:33 +0300

    Latest lesson changes

--- Assets/Scripts/MainMenuConnectionManager.cs
+++ Assets/Scripts/MainMenuConnectionManager.cs
@@ -1,15 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.Net.Mime;
 using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class MainMenuConnectionManager : MonoBehaviourPunCallbacks
 {
     private const string LOBBY_DEFAULT_NAME = "OurCoolLobby";
+    private const string ROOM_DEFAULT_NAME = "OurCoolRoom";
+    
     [SerializeField] private TextMeshProUGUI debugPhotonText;
 
+    [SerializeField] private TMP_InputField roomNameInputField;
+
+    [SerializeField] private Button[] joinRoomButtons;
     public void Connect()
     {
         PhotonNetwork.ConnectUsingSettings();
@@ -19,17 +27,35 @@ public class MainMenuConnectionManager : MonoBehaviourPunCallbacks
     {
         Debug.Log("We successfuly connected to Photon");
         base.OnConnectedToMaster();
+        ToggleJoinRoomButtonsState(true);
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        base.OnDisconnected(cause);
+      //  cause == 
     }
 
     public void CreateRoom()
     {
-        PhotonNetwork.CreateRoom("MyRoom");
+        RoomOptions roomOptions = new RoomOptions()
+        {
+            MaxPlayers = 4,
+        };
+        PhotonNetwork.CreateRoom("MyRoom",roomOptions);
+        ToggleJoinRoomButtonsState(false);
     }
 
     public override void OnCreatedRoom()
     {
         base.OnCreatedRoom();
         Debug.Log("Room created successfully!");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        base.OnCreateRoomFailed(returnCode, message);
+        ToggleJoinRoomButtonsState(true);
     }
 
     public void JoinLobby()
@@ -46,12 +72,38 @@ public class MainMenuConnectionManager : MonoBehaviourPunCallbacks
     public void JoinRandomRoom()
     {
         PhotonNetwork.JoinRandomRoom();
+        ToggleJoinRoomButtonsState(false);
+    }
+
+    public void JoinRoomByName()
+    {
+        PhotonNetwork.JoinRoom(roomNameInputField.text);
+        ToggleJoinRoomButtonsState(false);
+    }
+
+    public void JoinOrCreateRoom()
+    {
+        PhotonNetwork.JoinOrCreateRoom(roomNameInputField.text, null, null);
+        ToggleJoinRoomButtonsState(false);
     }
 
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
         Debug.Log("We successfully joined the room " + PhotonNetwork.CurrentRoom);
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        base.OnJoinRoomFailed(returnCode, message);
+        Debug.LogError($"We couldn't join the room because {message} return code is {returnCode}");
+        ToggleJoinRoomButtonsState(true);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        base.OnJoinRandomFailed(returnCode, message);
+        ToggleJoinRoomButtonsState(true);
     }
 
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
@@ -63,8 +115,21 @@ public class MainMenuConnectionManager : MonoBehaviourPunCallbacks
         }
     }
 
+    private void Start()
+    {
+        ToggleJoinRoomButtonsState(false);
+    }
+
     private void Update()
     {
         debugPhotonText.text = PhotonNetwork.NetworkClientState.ToString();
+    }
+
+    private void ToggleJoinRoomButtonsState(bool active)
+    {
+        foreach (Button joinRoomButton in joinRoomButtons)
+        {
+            joinRoomButton.interactable = active;
+        }
     }
 }
